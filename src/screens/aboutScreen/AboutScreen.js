b93aecbcc4211766f2ea2dd3d3d920643a839122
Chaitanya-Dewangan <gtxwasudewangan@gmail.com>
2021-07-29 12:08:35 +0530

    COMPLETED YOUTUBE-CLONE WITH YOUTUBE API

---
+++ src/screens/aboutScreen/AboutScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container} from "react-bootstrap";
+
+const AboutScreen = () => {
+    return (
+        <Container>
+            <h1 className="h1">Devloper @ChaitanyaDewangan</h1>
+            <p>-----------------------------------------------------</p>
+            
+        </Container>
+    )
+}
+
+export default AboutScreen
